b60809bedca23a2c0bf69fc57eab6cca0212eb0e
Zach Buth <zachbuth@gmail.com>
2023-08-09 16:04:42 -0400

    1st

---
+++ Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Coffee_Shop_Registration.Models;
+
+namespace Coffee_Shop_Registration.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Registration()
+    {
+        return View();
+    }
+
+    public IActionResult Summary(NewUser x)
+    {
+        return View(x);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Coffee Shop Registration/Models/NewUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Coffee_Shop_Registration.Models
+{
+	public class NewUser
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
+
