faf0feb64238f7e5c1e071335fdfaab141975554
Zach Buth <zachbuth@gmail.com>
2023-08-11 16:18:13 -0400

    Added Products

---
+++ Coffee Shop Registration/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Coffee_Shop_Registration.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Coffee_Shop_Registration.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CoffeeDbContext _coffeeDbContext = new CoffeeDbContext();
+
+        public IActionResult Index()
+        {
+            List<Product> result = _coffeeDbContext.Product.ToList();
+           
+            return View(result);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Product p = _coffeeDbContext.Product.FirstOrDefault(x => x.Id == id);
+            return View(p);
+        }
+    }
+}
+
---
+++ Coffee Shop Registration/Models/CoffeeDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee_Shop_Registration.Models;
+
+public partial class CoffeeDbContext : DbContext
+{
+    public CoffeeDbContext()
+    {
+    }
+
+    public CoffeeDbContext(DbContextOptions<CoffeeDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Product { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433; Initial Catalog=CoffeeDb; User ID=sa; Password=someThingComplicated1234; TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3213E83FAA35851D");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Category).HasMaxLength(255);
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
